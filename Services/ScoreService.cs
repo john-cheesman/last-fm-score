25215fb5758271bdc46cf6c03bea2b9d93199dd2
John Cheesman <me@johncheesman.org.uk>
2020-08-08 20:15:13 +0100

    Run API calls in parallel

--- Services/ScoreService.cs
+++ Services/ScoreService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using LastFMScore.Models;
 
@@ -24,17 +25,25 @@ namespace LastFMScore.Services
 
         public async Task<int> GetScore(List<Artist> artists)
         {
-            Console.WriteLine("Get score");
-            List<Tag> allTags = new List<Tag>();
-
-            foreach (Artist artist in artists)
+            try
             {
-                List<Tag> tags = await _lastFMService.GetTagsForArtist(artist.Name, 3, _excludedTags);
+                Console.WriteLine("Get score");
+                List<Tag> allTags = new List<Tag>();
+                var tasks = artists.Select(artist => _lastFMService.GetTagsForArtist(artist.Name, 3, _excludedTags));
+                var tags = await Task.WhenAll(tasks);
 
-                allTags = allTags.Union(tags, new TagComparer()).ToList();
+                foreach (List<Tag> tag in tags)
+                {
+                    allTags = allTags.Union(tag, new TagComparer()).ToList();
+                }
+
+                return allTags.Count();
             }
-
-            return allTags.Count();
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting score: {ex.ToString()}");
+                return -1;
+            }
         }
     }
 }
